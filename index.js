8d7f4887d5ecda7b574381e202cf78980978942e
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-09 12:39:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object,key){
-  var newObject = Object.assign ({}, object)
+  var newObject = Object.assign ( {}, object, [key] )
   delete newObject.key
   return newObject
 }
